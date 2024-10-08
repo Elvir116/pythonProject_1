accf37528c9188ee22c6d33db0ada3aa243936b1
Elvir116 <ter2909@yandex.ru>
2024-10-08 21:44:46 +0300

    commit7

--- module_2.2.py
+++ module_2.2.py
@@ -1,7 +1,7 @@
 first = int(input('first: '))
 second = int(input('second: '))
 third = int(input('third: '))
-if first == second and second == third:
+if first <= second:
     print(3)
 elif first == second or second == third or third == first:
     print(2)
